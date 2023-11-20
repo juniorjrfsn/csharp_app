e069e26b723b7be5d9fa3189bb239466e4926415
juniorjrfsn <junior.jrfsn@gmail.com>
2023-11-20 14:19:01 -0400

    ...
    ...

---
+++ RepositoryServicePatternDemo.Core/Services/Interfaces/ITicketService.cs
@@ -0,0 +1,10 @@
+﻿using RepositoryServicePatternDemo.Core.Models;
+using System.Collections.Generic;
+
+namespace RepositoryServicePatternDemo.Core.Services.Interfaces
+{
+    public interface ITicketService
+    {
+        List<Ticket> GetAllSold();
+    }
+}
---
+++ RepositoryServicePatternDemo.Core/Services/TicketService.cs
@@ -0,0 +1,19 @@
+﻿using RepositoryServicePatternDemo.Core.Models;
+using RepositoryServicePatternDemo.Core.Repositories.Interfaces;
+using RepositoryServicePatternDemo.Core.Services.Interfaces;
+using System.Collections.Generic;
+
+namespace RepositoryServicePatternDemo.Core.Services
+{
+    public class TicketService : ITicketService
+    {
+        private readonly ITicketRepository _ticketRepo;
+
+        public TicketService(ITicketRepository ticketRepo)
+        {
+            _ticketRepo = ticketRepo;
+        }
+
+        public List<Ticket> GetAllSold() => _ticketRepo.GetAllSold();
+    }
+}
